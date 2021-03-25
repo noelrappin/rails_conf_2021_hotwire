29a925d0c3c7f539b999814b91b6963189ab7e71
Noel Rappin <noelrap@hey.com>
2021-03-24 21:21:37 -0500

    3
    # Conflicts:
    #	app/controllers/favorites_controller.rb

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,14 +3,15 @@ class FavoritesController < ApplicationController
   end
 
   def create
-    Favorite.create(user: current_user, concert_id: params[:concert_id])
-    render(partial: "favorites/list", locals: {user: current_user})
+    @favorite = Favorite.create(
+      user: current_user,
+      concert_id: params[:concert_id]
+    )
   end
 
   def destroy
     @favorite = Favorite.find(params[:id])
     @favorite.destroy
-    render(partial: "favorites/list", locals: {user: current_user})
   end
 
   private def favorite_params
