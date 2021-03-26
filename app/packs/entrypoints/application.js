d150042ac0a3c6b5674b641c1fe0ac5872859777
Noel Rappin <noelrap@hey.com>
2021-03-25 21:23:21 -0500

    Commit

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -4,11 +4,10 @@ import "core-js/stable"
 import "regenerator-runtime/runtime"
 import * as ActiveStorage from "@rails/activestorage"
 import Rails from "@rails/ujs"
+import "controllers"
 
 Rails.start()
 ActiveStorage.start()
 
 const images = require.context("../images", true)
 const imagePath = (name) => images(name, true)
-
-import "controllers"
